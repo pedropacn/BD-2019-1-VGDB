c3306720d3865ce698cdbe6498971f631c3225c0
ACSouzaJr <junioracs2000@hotmail.com>
2019-06-27 16:49:52 -0300

    feat: add homepage

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@
 # third-party imports
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 # from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
 
-# db variable initialization
+# variables initialization
+login_manager = LoginManager()
 # db = SQLAlchemy()
 
 
@@ -16,6 +18,9 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
     
     # db.init_app(app)
     Bootstrap(app)
@@ -27,9 +32,10 @@ def create_app(config_name):
     from .dog import dog as dog_blueprint
     app.register_blueprint(dog_blueprint)
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-      return 'Hello, World!'
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .home import home as home_blueprint
+    app.register_blueprint(home_blueprint)
 
     return app
