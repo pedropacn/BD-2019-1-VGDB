e96aae6cb13924580621dae78a65f37aeb74c176
ACSouzaJr <junioracs2000@hotmail.com>
2019-06-27 22:27:26 -0300

    feat: add registrantion and login

--- app/__init__.py
+++ app/__init__.py
@@ -18,24 +18,27 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-    login_manager.init_app(app)
-    login_manager.login_message = "You must be logged in to access this page."
-    login_manager.login_view = "auth.login"
+
+
     
     # db.init_app(app)
     Bootstrap(app)
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
 
     from app import models
 
 
     # blueprint registration
-    from .dog import dog as dog_blueprint
-    app.register_blueprint(dog_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+    
+    from .dog import dog as dog_blueprint
+    app.register_blueprint(dog_blueprint)
 
     return app
---
+++ app/auth/views.py
@@ -0,0 +1,81 @@
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required, login_user, logout_user
+
+from . import auth
+from .forms import LoginForm, RegistrationForm
+
+from app.models import User
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    Handle requests to the /register route
+    Add an employee to the database through the registration form
+    """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User()
+
+        new_user = {
+            "email":form.email.data,
+            "username":form.username.data,
+            "first_name":form.first_name.data,
+            "last_name":form.last_name.data,
+            "password": user.password_hash(form.password.data)
+                }
+
+        # add user to the database
+        user.create(**new_user)
+        
+        flash('You have successfully registered! You may now login.')
+
+        # redirect to the login page
+        return redirect(url_for('auth.login'))
+
+    # load registration template
+    return render_template('auth/register.html', form=form, title='Register')
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Handle requests to the /login route
+    Log an employee in through the login form
+    """
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        # check whether employee exists in the database and whether
+        # the password entered matches the password in the database
+        cur = User()
+        user = cur.filter_by(email=form.email.data)
+        print(user)
+        print(cur.verify_password(user['password'], form.password.data))
+        if user is not None and cur.verify_password(user['password'], form.password.data):
+            # log employee in
+            
+
+            # redirect to the dashboard page after login
+            return redirect(url_for('dog.index'))
+
+        # when login details are incorrect
+        else:
+            flash('Invalid email or password.')
+
+    # load login template
+    return render_template('auth/login.html', form=form, title='Login')
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    """
+    Handle requests to the /logout route
+    Log an employee out through the logout link
+    """
+    logout_user()
+    flash('You have successfully been logged out.')
+
+    # redirect to the login page
+    return redirect(url_for('auth.login'))
